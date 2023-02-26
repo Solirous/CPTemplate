c4f5af24daa0e3cc022cd4ec599c8888d3a11528
Solirous <99412371+Solirous@users.noreply.github.com>
2023-02-26 14:41:34 +0700

    hi2

---
+++ README.md
@@ -0,0 +1,1 @@
+hi guys my cp temp
---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastIO cin.tie(0)->sync_with_stdio(0)
+#define forn(i,n) for(int i=0;i<int(n);i++)
+
+void debug(vector<int>& pr){
+    for(int i=0;i<pr.size();i++){
+        cout<<pr[i]<<" ";
+    }
+    cout<<"\n";
+}
+
+
+int t=1;
+void solve(){
+
+
+}
+
+main(){
+    fastIO;
+    if(t!=1){
+    cin>>t;
+    }
+    while(t--){
+        solve();
+    }
+    return 0;
+}
